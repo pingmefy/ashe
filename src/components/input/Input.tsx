744922bd7a08eafd2e485f820c28eac6be93a334
Alvaro Falcon Morales <alvaro.falconm93@gmail.com>
2024-05-29 19:25:33 +0100

    input styling

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -3,7 +3,7 @@ import {InputProps} from "./Input.types";
 export const Input = ({type, value, onChange, placeholder}: InputProps) => {
   return (
     <input
-      className={"text-primaryColor rounded-[10px] border-solid border border-highlightColor px-[17px] py-[14.5px] w-2/4"}
+      className={"text-primaryColor rounded-[10px] border-solid border-2 border-highlightColor px-[17px] py-[14.5px] w-2/4"}
       type={type}
       value={value}
       onChange={(event) => onChange(event.target.value)}
