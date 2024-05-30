8c1c43dfe476112552059c22ae3ab45fba046417
Alvaro Falcon Morales <alvaro.falconm93@gmail.com>
2024-05-30 11:33:48 +0100

    landing styling

--- src/components/UnAuthBlock.tsx
+++ src/components/UnAuthBlock.tsx
@@ -6,7 +6,7 @@ export const UnAuthBlock = () => {
   const [profileUrl, setProfileUrl] = useState("");
   return(
     <div className={"bg-primaryColorDark border-4 border-highlightColor flex flex-col items-center justify-center m-10"}>
-      <div className={"mt-10 w-2/4"}>
+      <div className={"mt-11 w-2/4"}>
         <Input onChange={(value) => setProfileUrl(value)} value={profileUrl} type={"text"} placeholder={"Your profile url"}/>
       </div>
       <Button onClick={() => null}>select my friends</Button>
