aa88a077a36802ab7cf96792ab1841576f7988a1
Alvaro Falcon Morales <alvaro.falconm93@gmail.com>
2024-05-31 16:30:32 +0100

    creatd app

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../styles/globals.css'
+import Home from "./index";
+
+function MyApp() {
+  return <Home />
+}
+
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import App from "@components/App";
 import React from 'react';
 import 'react-roulette-pro/dist/index.css';
-import "../styles/globals.css";
 import {AppProvider} from "../context/AppContext";
 
 const Home = () => {
