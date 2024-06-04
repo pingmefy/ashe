e90d7433d6c5df257554caf261fa0abd469ae6d0
Alvaro Falcon Morales <alvaro.falconm93@gmail.com>
2024-06-04 13:22:21 +0100

    added title and meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
 import App from "@components/App";
+import Head from "next/head";
 import React from 'react';
 import 'react-roulette-pro/dist/index.css';
 import {AppProvider} from "../context/AppContext";
 
 const Home = () => {
   return (
-    <div>
-      <AppProvider>
-        <App/>
-      </AppProvider>
-    </div>
+    <>
+      <Head>
+        <title>letsplayto</title>
+        <meta name='impact-site-verification' content='9120e37b-5149-4abb-9a84-4c951a1f5ec4'/>
+      </Head>
+      <main>
+        <div>
+          <AppProvider>
+            <App/>
+          </AppProvider>
+      </div>
+    </main>
+    </>
   );
 }
 
