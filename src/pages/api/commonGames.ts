0d8c49cbc6ac52cf59a6812c5dd1402e2453b860
Alvaro Falcon Morales <alvaro.falconm93@gmail.com>
2024-06-06 14:34:34 +0100

    sending proper error if needed

--- src/pages/api/commonGames.ts
+++ src/pages/api/commonGames.ts
@@ -3,6 +3,9 @@ import type {NextApiRequest, NextApiResponse} from 'next';
 import {logger} from "../../util/logger";
 import {APIGameResponse} from "../../util/types";
 
+const PRIVACY_ERROR_MESSAGE = "One or more of your friends has a privacy setting that doesn't allow you to see \"their games.\"\n" +
+  "We show you what games you could play with the others, more info here."
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -27,7 +30,7 @@ export default async function handler(
     }))
     const response = {
       data: result,
-      error: privacyError || null
+      error: privacyError ? PRIVACY_ERROR_MESSAGE : null
     }
     res.status(200).json(response)
 
