63d65b2af24f2e00bbc695cce5b2ab71286e93f6
Alvaro Falcon Morales <alvaro.falconm93@gmail.com>
2024-06-11 16:21:36 +0100

    fix typo

--- src/components/BodyTitle.tsx
+++ src/components/BodyTitle.tsx
@@ -2,7 +2,7 @@ export const BodyTitle = () => {
   return (
     <h1 className="text-lg text-white p-4 m-0 text-center font-bold">
       we will recommend a <span className={"text-highlightColor"}>game</span> that
-      you can ply with your <span className={"text-highlightColor"}>friends</span>!
+      you can play with your <span className={"text-highlightColor"}>friends</span>!
     </h1>
   )
 }
