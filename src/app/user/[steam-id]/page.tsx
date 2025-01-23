50ace35300f08d759ca4e201a5b8b48c775c2c02
Alvaro Falcon Morales <alvaro.falconm93@gmail.com>
2025-01-23 16:25:05 +0000

    fix style

--- src/app/user/[steam-id]/page.tsx
+++ src/app/user/[steam-id]/page.tsx
@@ -74,7 +74,7 @@ const UserPage = async (props: Props) => {
             <div
               className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4`}
             >
-              {friends.map((friend, index) => {
+              {friends.map((friend) => {
                 return (
                   <FriendItem
                     key={friend.steamID}
