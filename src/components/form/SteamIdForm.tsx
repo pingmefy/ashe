d45570ef130d0ca58e6f9a51511c09a9255bb64c
Alvaro Falcon Morales <alvaro.falconm93@gmail.com>
2025-01-23 17:16:41 +0000

    fix: let to const

--- src/components/form/SteamIdForm.tsx
+++ src/components/form/SteamIdForm.tsx
@@ -1,31 +1,36 @@
-"use client"
-import {useCallback, useState} from "react";
+"use client";
+import { useCallback, useState } from "react";
 import getSteamId from "../../actions/get-steam-id";
-import {Button} from "../button/Button";
-import {Input} from "../input/Input";
+import { Button } from "../button/Button";
+import { Input } from "../input/Input";
 
 const SteamIdForm = () => {
   const [profileUrl, setProfileUrl] = useState("");
   const handleButtonClick = () => {
-    getSteamId(profileUrl)
-  }
+    getSteamId(profileUrl);
+  };
 
   const handleOnInputChange = useCallback((value: string) => {
-    let url = value.trim();
-    setProfileUrl(url)
-  },[])
+    setProfileUrl(value.trim);
+  }, []);
 
   return (
     <div
-      className={"bg-primaryColorDark border-4 border-highlightColor flex flex-col items-center justify-center "}>
+      className={
+        "bg-primaryColorDark border-4 border-highlightColor flex flex-col items-center justify-center "
+      }
+    >
       <div className={"mt-12 w-full px-12"}>
-        <Input onChange={(value) => handleOnInputChange(value)}
-               value={profileUrl} type={"text"}
-               placeholder={"https://steamcommunity.com/id/yourusername/"}/>
+        <Input
+          onChange={(value) => handleOnInputChange(value)}
+          value={profileUrl}
+          type={"text"}
+          placeholder={"https://steamcommunity.com/id/yourusername/"}
+        />
       </div>
       <Button onClick={handleButtonClick}>select my friends</Button>
     </div>
-  )
-}
+  );
+};
 
 export default SteamIdForm;
